2f2377c82332732480dddddea443f077d094b2d9
Zachary Sang <zacharysang@gmail.com>
2018-08-10 12:51:19 +0000

    Uses listener flow instead of control
    Use listeners inherent flow instead of using control mechanisms (if/for)

--- webapp_src/theDrawLListener.js
+++ webapp_src/theDrawLListener.js
@@ -7,7 +7,6 @@ let DrawLListener = require('../target/generated-sources/antlr4/com/zacharysang/
 
 // make a constructor that inherits from the generated listener
 function theDrawLListener(canvas) {
-    
     this.canvas = canvas;
     
     // inherit from the generated listener
@@ -23,92 +22,66 @@ theDrawLListener.prototype.constructor = theDrawLListener;
 // override the listener functions (there is a listener for each parser rule)
 
 // each streak should handle initializing a new drawing line
-theDrawLListener.prototype.exitStreak = function (ctx) {
+theDrawLListener.prototype.enterStreak = function(streak) {
     this.variables = {};
         
     // get the location
-    let coords = ctx.location().NUMBER();
-    let x = parseInt(coords[0].getText());
-    let y = parseInt(coords[1].getText());
+    let coords = streak.location().NUMBER();
+    this.x = parseInt(coords[0].getText());
+    this.y = parseInt(coords[1].getText());
     
     this.canvas.beginPath();
-    this.canvas.moveTo(x,y);
-    
-    let newX = x;
-    let newY = y;
-    
-    // draw based on tvecs
-    let tvecs = ctx.tvec();
-    
-    // loop through all tvecs in the streak
-    for (idx in tvecs) {
-        let tvec = tvecs[idx];
-            
-        let it = tvec.NUMBER() || 1;
-        
-        let angle = 0;
-        let magnitude = 1;
-        
-        // set up styling
-        if (tvec.style()) {
-            let style = tvec.style();
-            this.canvas.lineWidth = style.NUMBER() || 1;
-            this.canvas.strokeStyle = style.COLOR() || '#000000';
-        } else {
-            this.canvas.lineWidth = 1;
-            this.canvas.strokeStyle = '#000000';
-        }
-        
-        // set up declarations if any on tvec
-        let declaration = tvec.declaration();
-        if(declaration) {
-            for (let idx in declaration) {
-                let dec = declaration[idx];
-                
-                this.variables[dec.VAR().getText()] = [];
-                let start = dec.NUMBER()[0];
-                let end = dec.NUMBER()[1];
-                if (start < end) {
-                    for (let i = start; i < end; i++) {
-                        this.variables[dec.VAR().getText()].push(i);
-                    }
-                } else {
-                    for (let i = start; i > end; i--) {
-                        this.variables[dec.VAR().getText()].push(i);
-                    }
-                }
-                
-            }
-        }
-    
-        // loop through all iterations of this tvec
-        for (let i = 0; i < it; i++) {
-            
-            // convert the angle given in degrees into radians
-            let angleVal = tvec.value()[0];
-            let angleDeg = angleVal.VAR() ? this.variables[angleVal.VAR().getText()].shift() : angleVal.NUMBER();
-            angle += (Math.PI * angleDeg) / 180;
-            
-            let magnitudeVal = tvec.value()[1];
-            magnitude = magnitudeVal.VAR() ? this.variables[angleVal.VAR().getText()].shift() : magnitudeVal.NUMBER();
-            
-            newX += (Math.cos(angle) * magnitude);
-            newY += (Math.sin(angle) * magnitude);
-            
-            this.canvas.lineTo(newX, newY);    
-            this.canvas.moveTo(newX, newY);
-            
-            this.canvas.stroke();
-        }
-    }
-    
+    this.canvas.moveTo(this.x, this.y);
+};
+theDrawLListener.prototype.exitStreak = function(streak) {
     this.canvas.closePath();    
 };
 
+theDrawLListener.prototype.exitTvec = function(tvec) {
+    let it = tvec.NUMBER() || 1;
+        
+    let angle = 0;
+    let magnitude = 1;
+    
+    // loop through all iterations of this tvec
+    for (let i = 0; i < it; i++) {
+            
+        // convert the angle given in degrees into radians
+        let angleVal = tvec.value()[0];
+        let angleDeg = angleVal.VAR() ? this.variables[angleVal.VAR().getText()].shift() : angleVal.NUMBER();
+        angle += (Math.PI * angleDeg) / 180;
+            
+        let magnitudeVal = tvec.value()[1];
+        magnitude = magnitudeVal.VAR() ? this.variables[angleVal.VAR().getText()].shift() : magnitudeVal.NUMBER();
+            
+        this.x += Math.cos(angle) * magnitude;
+        this.y += Math.sin(angle) * magnitude;
+        
+        this.canvas.lineTo(this.x, this.y);    
+        this.canvas.moveTo(this.x, this.y);
+            
+        this.canvas.stroke();
+    }
+};
 
-theDrawLListener.prototype.enterLocation = function(ctx) {
-}
+theDrawLListener.prototype.enterStyle = function(style) {
+    this.canvas.lineWidth = style.NUMBER() || 1;
+    this.canvas.strokeStyle = style.COLOR() || '#000000';
+};
 
+theDrawLListener.prototype.enterDeclaration = function(declaration) {
+    this.variables[declaration.VAR().getText()] = [];
+    let start = declaration.NUMBER()[0];
+    let end = declaration.NUMBER()[1];
+    if (start < end) {
+        for (let i = start; i < end; i++) {
+            this.variables[declaration.VAR().getText()].push(i);
+        }
+    } else {
+        for (let i = start; i > end; i--) {
+            this.variables[declaration.VAR().getText()].push(i);
+        }
+    }
+};
 
-
-exports.theDrawLListener = theDrawLListener
+exports.theDrawLListener = theDrawLListener;
