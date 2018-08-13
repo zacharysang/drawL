1fc1b6c7336c669ca9568d8af19fe01bc003572e
Zachary Sang <zacharysang@gmail.com>
2018-08-13 14:46:56 +0000

    default tomin var length for iterations

--- webapp_src/parser/section.js
+++ webapp_src/parser/section.js
@@ -5,7 +5,19 @@
 const VALUE = require('./value');
 
 function enter(section) {
-    let it = section.NUMBER() || 1;
+    let vars = getVars(section);
+    
+    let it = 1;
+    
+    if (section.NUMBER()) {
+        it = section.NUMBER();
+    } else if (vars.length > 0) {
+        let varLengths = vars.map( (v) => {return this.variables[v].length;} );
+
+        let minVarLength = varLengths.reduce( (acc, val) => {return Math.min(acc, val);});
+    
+        it = minVarLength;
+    }
     
     // loop through all iterations of this section
     for (let i = 0; i < it; i++) {
@@ -24,6 +36,31 @@ function enter(section) {
         
         this.canvas.stroke();    
     }
+}
+
+function getVars(section) {
+    
+    // initialize a stack with the section values
+    let valueStack = [].concat(section.value());
+    
+    let vars = [];
+    
+    // add the section values
+    while (valueStack.length > 0) {
+        // pop and process the top of the stack
+        let val = valueStack.pop();
+        
+        if (val.VAR()) {
+            // if value is a variable, add it to the vars list
+            vars.push(val.VAR().getText());
+        } else if (val.expression()) {
+            // if value is an expression, then add its values to the stack
+            valueStack.push(val.expression().value()[0]);
+            valueStack.push(val.expression().value()[1]);
+        }
+    }
+    
+    return vars;
 }
 
 exports.enter = enter;
